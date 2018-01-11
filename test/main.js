9b697a06008337aab929aa01ca835d698c61156c
Vivek Kumar <vivek8.kumar@paytm.com>
2018-01-12 04:25:31 +0530

    Updated Readme file. Bug Fix : 1. Issue in resolving relative path. 2. Handling for home is missing while creating AppManager instance 3. Safe Handling while calling BootAppListener init method

--- test/main.js
+++ test/main.js
@@ -7,7 +7,7 @@ var AppManager = require('../lib/AppManager');
 (function () {
     if (require.main === module) {
         new AppManager({
-            home : __dirname
+            home : __dirname    //From v2.0.* onwards you can use APP_HOME environment variable to define home
             //optional
 //            ,directoryFilter : function(dir){
 //                return !(dir === __dirname+"/lib") && !(dir === __dirname+"/node_modules");
